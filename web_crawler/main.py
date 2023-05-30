6e0d6c241437685f8964387f1ae02329dad97e6e
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-30 18:24:50 -0300

    Starting changes on CSS. Theme to gruvbox.

--- web_crawler/main.py
+++ web_crawler/main.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
