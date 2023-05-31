f6696c2e90450a2007d96c87f9e216396197b20c
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-31 19:08:26 -0300

    (gruvybox) Create new user page with theme, refactoring css.

--- WikiBook/social_media/static/social_media/main.js
+++ WikiBook/social_media/static/social_media/main.js
@@ -1,19 +1,19 @@
-function trigger () {
-    activate_typing_effect();
+function trigger() {
+  activate_typing_effect();
 }
 
 async function activate_typing_effect() {
-    elements = document.querySelectorAll('.typed-text');
-    for( el of elements ){
-        txt = el.innerText;
-        el.innerText = '';
-        await typing(el, txt);
-    }
+  elements = document.querySelectorAll(".typed-text");
+  for (el of elements) {
+    txt = el.innerText;
+    el.innerText = "";
+    await typing(el, txt);
+  }
 }
 
 function typing(el, txt) {
-  return new Promise(resolve => {
-    el.style.display = 'block';
+  return new Promise((resolve) => {
+    el.style.display = "block";
     let i = 0;
     const speed = 70;
 
@@ -23,7 +23,7 @@ function typing(el, txt) {
         i++;
         setTimeout(typer, speed);
       } else {
-        el.classList.add('final-typed-text');
+        el.classList.add("final-typed-text");
         resolve();
       }
     }
@@ -32,3 +32,10 @@ function typing(el, txt) {
   });
 }
 
+function remove_field_errors_dialog() {
+  dialog_error = document.querySelector(".error-form");
+  back_wall = document.querySelector(".back-wall");
+
+  dialog_error.remove();
+  back_wall.remove();
+}
